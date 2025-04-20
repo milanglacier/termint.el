95d1fc9f1b01dff7cb713a95bb5c4ee82c2390d6
Milan Glacier <dev@milanglacier.com>
2025-04-20 14:09:26 -0400

    chore: add license in package description.

--- termint.el
+++ termint.el
@@ -6,6 +6,25 @@
 ;; URL: https://github.com/milanglacier/termint.el
 ;; Package-Requires: ((emacs "29"))
 
+;;; This file is NOT part of GNU Emacs
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file LICENSE.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
 ;;; Commentary:
 
 ;; This package offers macros and functions for creating and managing
